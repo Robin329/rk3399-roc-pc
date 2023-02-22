fed4b74a1a20829de3909bd1c8ec5f4b568ea666
robin <jiangrenbin329@gmail.com>
2023-02-22 17:23:47 +0000

    update build scripts
    Signed-off-by: robin <jiangrenbin329@gmail.com>

--- build_rock5b.py
+++ build_rock5b.py
@@ -50,7 +50,7 @@ def build_kernel(command_one):
     if "build_kernel" == command_one:
         os.system("make O=build roc-rk3399-pc_defconfig")
         print("===============" + '\033[1;33m' + "Start Build Image" + '\033[0m' + "===============")
-        ret = os.system("make Image -j32 O=build  2>&1 | tee build_Image.log")
+        ret = os.system("make -j32 O=build  2>&1 | tee build_Image.log")
         if ret == 0:
             print("Compile Finished !!!")
         print("===============" + '\033[1;33m' + "End Build Image" + '\033[0m' + "===============")
@@ -64,8 +64,8 @@ def build_kernel(command_one):
         print("===============" + '\033[1;33m' + "END Build modules" + '\033[0m' + "===============")
         if os.path.isdir("/home/rock/tftpboot"):
             print("tftpboot is exist!")
-            os.system("cp build/arch/arm64/boot/Image ~/tftpboot/")
-            os.system("cp build/arch/arm64/boot/dts/rockchip/rk3399-roc-pc.dtb /home/rock/tftpboot/")
+            os.system("sudo cp build/arch/arm64/boot/Image /home/rock/tftpboot/")
+            os.system("sudo cp build/arch/arm64/boot/dts/rockchip/rk3399-roc-pc.dtb /home/rock/tftpboot/")
             print("copy rk3399-roc-pc.dtb finish!")
             print("copy Image finish!")
         else:
@@ -80,15 +80,15 @@ def build_kernel(command_one):
         os.system("make dtbs -j32 O=build  2>&1 | tee build_dtbs.log")
         if os.path.isdir("/home/rock/tftpboot"):
             print("tftpboot is exist!")
-            os.system("cp build/arch/arm64/boot/dts/rockchip/rk3399-roc-pc.dtb /home/rock/tftpboot/")
+            os.system("sudo cp build/arch/arm64/boot/dts/rockchip/rk3399-roc-pc.dtb /home/rock/tftpboot/")
             print("copy rk3399-roc-pc.dtb finish!")
     elif "Image" == command_one:
-        os.system("make Image -j32 O=build  2>&1 | tee build_Image.log")
+        os.system("make -j32 O=build  2>&1 | tee build_Image.log")
         if os.path.isdir("/home/rock/tftpboot"):
             print("tftpboot is exist!")
-            os.system("cp build/arch/arm64/boot/Image /home/rock/tftpboot/")
+            os.system("sudo cp build/arch/arm64/boot/Image /home/rock/tftpboot/")
             print("copy Image to tftpboot dir finish!")
-        os.system("cd build/ && python ../scripts/clang-tools/gen_compile_commands.py && cd ../")
+        os.system("python ./scripts/clang-tools/gen_compile_commands.py -d build")
     elif "clean" == command_one:
         finish_1 = os.system("make clean O=build")
         finish_2 = os.system("make mrproper O=build")
