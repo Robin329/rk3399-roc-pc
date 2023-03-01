8f1c77803ce14b2a1944eb6879012e8b4d6490fe
robin <jiangrenbin329@gmail.com>
2023-03-01 13:16:05 +0000

    feat:add dma buf.
    Signed-off-by: robin <jiangrenbin329@gmail.com>

---
+++ build_rock.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+make O=build roc-rk3399-pc_defconfig
+make dtbs -j32 O=build
+sudo cp build/arch/arm64/boot/dts/rockchip/rk3399-roc-pc.dtb /home/rock/tftpboot/
+make -j32 O=build
+sudo cp build/arch/arm64/boot/Image /home/rock/tftpboot/
+ls -alh  /home/rock/tftpboot/*
+python ./scripts/clang-tools/gen_compile_commands.py -d build
